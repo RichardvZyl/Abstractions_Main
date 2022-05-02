5f9bb44c79c5b3a0ee977d92fc817c1baf1c70f5
RichardvZyl <richardvzyl@gmail.com>
2022-05-02 11:37:53 +0200

    Added interface that allows for implementation of EndpointDefinitions in minimal API's

---
+++ IoC/IEndpointDefintion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Abstractions.IoC;
+
+public interface IEndpointDefintion
+{
+    public void DefineEndpoints(WebApplication app);
+
+    public void DefineServices(IServiceCollection services);
+
+}
