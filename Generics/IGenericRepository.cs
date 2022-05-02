1651164a0225972736b9fd03cf28cbe26526e670
RichardvZyl <richardvzyl@gmail.com>
2022-05-02 13:05:30 +0200

    Added return type (success: bool) so it can be used to check for errors

--- Generics/IGenericRepository.cs
+++ Generics/IGenericRepository.cs
@@ -5,8 +5,8 @@ public interface IGenericRepository<T, TId>
     Task<TId> Create(T model);
     Task<T> GetById(TId id);
     Task<IEnumerable<T>> ReadList();
-    Task Update(T model);
-    Task Delete(TId id);
+    Task<bool> Update(T model);
+    Task<bool> Delete(TId id);
 
     Task<IDictionary<TId, T>> GetTable();
 }
