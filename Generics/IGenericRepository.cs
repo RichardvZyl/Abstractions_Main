1b50e5a5cd4695d529f175b443b994c456a7bb58
RichardvZyl <richardvzyl@gmail.com>
2022-05-02 11:14:16 +0200

    Added Generics

---
+++ Generics/IGenericRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Abstractions.Generics;
+public interface IGenericRepository<T, TId>
+{
+    Task<TId> Create(T model);
+    Task<IEnumerable<T>> ReadList();
+    Task Update(T model);
+    Task Delete(TId id);
+}
---
+++ Generics/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿using Abstractions.Results;
+
+namespace Abstractions.Generics;
+
+public interface IGenericService<T, TId>
+{
+    Task<IResult<TId>> Create(T model);
+    Task<IResult<IEnumerable<T>>> ReadList();
+    Task<IResult> Update(T model);
+    Task<IResult> Delete(TId id);
+}
