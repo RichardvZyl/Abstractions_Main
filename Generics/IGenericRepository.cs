579f12bee6844ebfd19592b5db347da27f8648cc
RichardvZyl <richardvzyl@gmail.com>
2022-05-02 11:34:52 +0200

    Added GetTable and GetById

--- Generics/IGenericRepository.cs
+++ Generics/IGenericRepository.cs
@@ -1,8 +1,12 @@
 ﻿namespace Abstractions.Generics;
+
 public interface IGenericRepository<T, TId>
 {
     Task<TId> Create(T model);
+    Task<T> GetById(TId id);
     Task<IEnumerable<T>> ReadList();
     Task Update(T model);
     Task Delete(TId id);
+
+    Task<IDictionary<TId, T>> GetTable();
 }
--- Generics/IGenericService.cs
+++ Generics/IGenericService.cs
@@ -5,7 +5,8 @@ namespace Abstractions.Generics;
 public interface IGenericService<T, TId>
 {
     Task<IResult<TId>> Create(T model);
-    Task<IResult<IEnumerable<T>>> ReadList();
+    Task<IResult<T>> Get(TId id);
+    Task<IResult<IEnumerable<T>>> GetList();
     Task<IResult> Update(T model);
     Task<IResult> Delete(TId id);
 }
