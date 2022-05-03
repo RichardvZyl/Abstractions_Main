308d56f2b2fa225bf6da5eef55ff4172d2f1a52c
RichardvZyl <richardvzyl@gmail.com>
2022-05-03 22:59:59 +0200

    Generics updated to include nullable return type for GetById

--- Generics/IGenericRepository.cs
+++ Generics/IGenericRepository.cs
@@ -5,7 +5,7 @@ namespace Abstractions.Generics;
 public interface IGenericRepository<TEntity, TId> where TEntity : Entity<TId> where TId : struct
 {
     Task<TId> CreateAsync(TEntity entity);
-    Task<TEntity> GetByIdAsync(TId id);
+    Task<TEntity?> GetByIdAsync(TId id);
     Task<IEnumerable<TEntity>> ListAsync();
     Task<bool> UpdateAsync(TEntity entity);
     Task<bool> DeleteAsync(TId id);
